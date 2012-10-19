c5ef0fc7ba49b31a66477856b598ba8735b96244
Brandon Ewoldt <bewoldt1@gmail.com>
2012-10-19 14:04:09 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+
+	/* dropdown */
+	$('.dropdown-toggle').dropdown();
+
+	/* jquery stickem init */
+	$('.body').stickem();
+
+	/* scroll spy */
+	$('.sub-nav').scrollspy();
+});
---
+++ js/plugins.js
@@ -0,0 +1,4 @@
+//@codekit-append "vendor/jquery-1.8.0.min.js"
+//@codekit-append "vendor/dropdown.min.js"
+//@codekit-append "vendor/jquery.stickem.js"
+//@codekit-append "vendor/bootstrap-scrollspy.js"
